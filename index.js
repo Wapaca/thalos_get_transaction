83b622c7937f2f80b26cdf9e305dddf4614776d6
Wapaca <wapaca@gmail.com>
2024-02-18 22:12:41 +0100

    Set transactions into redis + add api endpoint get code

--- index.js
+++ index.js
@@ -8,17 +8,41 @@ const thalos_client = thalos.createRedisClient({
     ns: "1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4" // wax mainnet
 })
 
-thalos_client.onTransaction(transaction => {
-    console.log("Transaction", transaction.id);
+thalos_client.onTransaction(async (transaction) => {
+    const key = 'thalos_transaction_'+transaction.id;
+    try {
+        await thalos_client.set(key, transaction, 'EX', 1200); // 20 minutes expiry
+    }
+    catch(e) {
+        console.log('Error adding key value to redis');
+        console.error(e);
+    }
 });
 
 const app = express()
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-app.get('/get_transaction_traces/:id', async (req, res) => {
-    res.json({ code: 404 })
+app.get('/get_transaction_traces/:txid', async (req, res) => {
+    const key = 'thalos_transaction_'+req.params.tx_id;
+    try {
+        const transaction = await thalos_client.get(key)
+
+        if(!transaction) {
+            res.json({code: 404, message: 'Transaction not found'})
+            return;
+        }
+
+        res.json(transaction)
+    }
+    catch(e) {
+        res.json({code: 500, message: 'Error while getting transaction'})
+    }
 });
 
 app.listen(8000, () => {
     console.log('Thalos get_transaction_traces wrapper listening on port 8000!')
 });
-
