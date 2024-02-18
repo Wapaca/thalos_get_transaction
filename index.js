5010efc802b67a88fc5bb3358ac4183652dba6e8
Wapaca <wapaca@gmail.com>
2024-02-18 22:16:23 +0100

    Add redis client package

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import * as thalos from '@eosswedenorg/thalos-client';
+import { createClient } from 'redis';
 
 // Create client.
 const thalos_client = thalos.createRedisClient({
@@ -8,10 +9,14 @@ const thalos_client = thalos.createRedisClient({
     ns: "1064487b3cd1a897ce03ae5b6a865651747e2e152090f99c1d19d44e01aea5a4" // wax mainnet
 })
 
+const redis_client = createClient({
+    url: "redis://127.0.0.1:6379"
+});
+
 thalos_client.onTransaction(async (transaction) => {
     const key = 'thalos_transaction_'+transaction.id;
     try {
-        await thalos_client.set(key, transaction, 'EX', 1200); // 20 minutes expiry
+        await redis_client.set(key, transaction, 'EX', 1200); // 20 minutes expiry
     }
     catch(e) {
         console.log('Error adding key value to redis');
@@ -29,7 +34,7 @@ app.use(function(req, res, next) {
 app.get('/get_transaction_traces/:txid', async (req, res) => {
     const key = 'thalos_transaction_'+req.params.tx_id;
     try {
-        const transaction = await thalos_client.get(key)
+        const transaction = await redis_client.get(key)
 
         if(!transaction) {
             res.json({code: 404, message: 'Transaction not found'})
